b76ae5e890d2b910c7dc4d39549a5b66d3ae48df
Romogna <alderon13@gmail.com>
2019-03-14 13:00:25 -0600

    converted to php

---
+++ catering.php
@@ -0,0 +1,47 @@
+<?php echo file_get_contents("html/header.html"); ?>
+
+<!-- content -->
+<div class="wrapper row2">
+  <div id="container" class="clear">
+
+    <!-- Slider -->
+    <section id="slider" class="clear">
+      <figure><img src="images/demo/630x300.gif" alt="">
+        <figcaption>
+          <h2>Try our Recipe Calculator!</h2>
+          <p>Our recipe calculator allow you to make perfectly sized proportions of any of our recipes we provide.</p>
+          <footer class="more"><a href="#">Try it out! &raquo;</a></footer>
+        </figcaption>
+      </figure>
+    </section>
+
+    <!-- main content -->
+    <div id="intro">
+      <section class="clear">
+
+        <!-- article 1 -->
+        <article class="two_quarter">
+          <h2>Catering Services</h2>
+          <p>We provice food catering to any event in the Albuquerque Area. To schedule an event please use the following link <a href="#" title="Free Website Templates">Catering</a>. For full terms of our services, please read our <a href="#">Terms of Service</a>.</p>
+
+          <p>Visit our forums for comments and further help with your cooking adventures. <a href="#">Forum</a>.</p>
+        </article>
+
+        <!-- article 2 -->
+        <article class="two_quarter lastbox">
+          <figure>
+            <ul class="clear">
+              <li><a href="#"><img src="images/demo/130x130.gif" width="130" height="130" alt=""></a></li>
+              <li><a href="#"><img src="images/demo/130x130.gif" width="130" height="130" alt=""></a></li>
+              <li class="last"><a href="#"><img src="images/demo/130x130.gif" width="130" height="130" alt=""></a></li>
+            </ul>
+            <figcaption><a href="#">View Our Image Gallery Here &raquo;</a></figcaption>
+          </figure>
+        </article>
+      </section>
+    </div>
+
+  </div>
+</div>
+
+<?php echo file_get_contents("html/footer.html"); ?>
